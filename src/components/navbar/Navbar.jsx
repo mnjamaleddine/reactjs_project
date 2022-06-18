82cbbd8e102c22699aedf97141e5d9a62d2fa340
Mazen Jamal-Eddine <mazenjamaleddine@hotmail.com>
2022-06-18 17:41:03 +0300

    commit;

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import {RiMenu3Line, RiCloseLine} from 'react-icons/ri';
+import logo from '../../assets_fix/logo.svg';
 import './navbar.css';
-
 
 const Navbar = () => {
   return (
     <div className='gpt3__navbar'>
       <div className='gpt3__navbar-links'>
         <div className='gtp3__navbar-links_logo'>
-          <img src="../../assets/logo.png" alt="logo" />
+          <img src={logo} alt="logo" />
         </div>
       </div>
     </div>
